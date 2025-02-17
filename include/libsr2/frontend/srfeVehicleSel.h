5d085a02f72a9be1ac1050a9ad546842def1388b
Michael DeCicco <mdecicco8@gmail.com>
2025-02-17 03:36:34 -0500

    started srfeVehicleSel

--- include/libsr2/frontend/srfeVehicleSel.h
+++ include/libsr2/frontend/srfeVehicleSel.h
@@ -1,16 +1,33 @@
 #pragma once
 #include <libsr2/types.h>
 #include <libsr2/frontend/srfeBasicLayout2.h>
+#include <libsr2/ui/ui2Master.h>
 
 namespace sr2 {
     class srfeVehicleSel : public srfeBasicLayout2 {
         public:
             srfeVehicleSel(
                 const char* name,
-                const char* p2,
-                const char* p3,
+                const char* prevScreenName,
+                const char* nextScreenName,
                 const WidgetRef<ui2Master>& master
             );
             ~srfeVehicleSel();
+        
+        protected:
+            struct Widgets {
+                WidgetRef<ui2Master> master;
+            };
+
+            const char* m_prevScreenName;
+            const char* m_nextScreenName;
+
+            undefined4 field_0xb8;
+            undefined4 field_0xc4;
+            undefined4 field_0xc8;
+
+            static Widgets* WidgetsInstance;
+            static u32 ActiveCount;
+            static u32 InitializedCount;
     };
 };
--- src/frontend/srfeVehicleSel.cpp
+++ src/frontend/srfeVehicleSel.cpp
@@ -1,9 +1,32 @@
 #include <libsr2/frontend/srfeVehicleSel.h>
 
 namespace sr2 {
-    srfeVehicleSel::srfeVehicleSel(const char* name, const char* p2, const char* p3, const WidgetRef<ui2Master>& master) : srfeBasicLayout2(name, master) {
+    srfeVehicleSel::Widgets* srfeVehicleSel::WidgetsInstance = nullptr;
+    u32 srfeVehicleSel::ActiveCount = 0;
+    u32 srfeVehicleSel::InitializedCount = 0;
+
+    srfeVehicleSel::srfeVehicleSel(
+        const char* name,
+        const char* prevScreenName,
+        const char* nextScreenName,
+        const WidgetRef<ui2Master>& master
+    ) : srfeBasicLayout2(name, master) {
+        m_prevScreenName = prevScreenName;
+        m_nextScreenName = nextScreenName;
+
+        field_0xb8 = 0;
+        field_0xc4 = 0;
+        field_0xc8 = 0;
+
+        if (m_isActive) ActiveCount++;
+        if (WidgetsInstance) {
+            if (ActiveCount == 1) WidgetsInstance->master->setActive(true);
+        }
     }
 
     srfeVehicleSel::~srfeVehicleSel() {
+        deinitScreen();
+
+        if (m_isActive) ActiveCount--;
     }
 };
